8c3c3d64e90bb81a8f773835df131aeb7a79cfa4
zzr202 <bbwats29@gmail.com>
2022-12-09 18:34:50 -0500

    Added limit switches for testing purposes

--- src/main/java/frc/robot/subsystems/SpinMotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/SpinMotorSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -12,19 +13,29 @@ private double m_speed = 0;
 
 CANSparkMax m_SpinMotor;
 
+DigitalInput toplimitSwitch = new DigitalInput(5);
+DigitalInput bottomlimitSwitch = new DigitalInput(4);
+
 public SpinMotorSubsystem(int spinMotor) {
     
 m_SpinMotor = new CANSparkMax (spinMotor, MotorType.kBrushless);
+
+
 
 }
 
 @Override
 public void periodic(){
     m_SpinMotor.set(m_speed);
+    if (!toplimitSwitch.get()){
+        System.out.println("pressed!");
+    }else {
+        System.out.println("Not pressed!");
+    }
 }
 
 public void spin(double speed){
-    m_speed = speed;
+        m_speed = speed;
 }
 
 
